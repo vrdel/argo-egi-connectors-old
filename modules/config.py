5d01f6986a81ba0b1c64af30ebe19edf291ce76b
Daniel Vrcic <daniel.vrcic@gmail.com>
2021-07-07 19:36:03 +0200

    allow dot and slash in topology filters

--- modules/config.py
+++ modules/config.py
@@ -428,7 +428,7 @@ class CustomerConf(object):
             if match is not None:
                 for m in match:
                     tags.update({m[0]: [e.strip('() ') for e in m[1].split(',')]})
-            match = re.findall('([\w]+)\s*:\s*([\w\.\-\_]+)', tagstr)
+            match = re.findall('([\w\.]+)\s*:\s*([\w\.\-\_\/]+)', tagstr)
             if match is not None:
                 for m in match:
                     tags.update({m[0]: m[1]})
